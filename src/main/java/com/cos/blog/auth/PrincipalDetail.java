6b45eace60b7d6d2bea0c76a3138237626483dda
Mint-Candy95 <32154068@dankook.ac.kr>
2022-05-03 16:06:14 +0900

    시큐리티 로그인 변경

--- src/main/java/com/cos/blog/auth/PrincipalDetail.java
+++ src/main/java/com/cos/blog/auth/PrincipalDetail.java
@@ -4,44 +4,59 @@ import com.cos.blog.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
-// 스프링
+// 스프링 시큐리티가 로그인 요청 가로채서 로그인 진행하고 완료되면 user details type의 오브젝트를
+// 스프링 시큐리티의 고유한 세션 저장소에 저장을 해준다.
 public class PrincipalDetail implements UserDetails {
     private User user; //콤포지션
 
+    public PrincipalDetail(User user){
+        this.user = user;
+    }
+
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getUsername();
     }
 
+    //계정 만료되었는지 (true = 만료안됨)
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
+    //계정 잠겨있는지 (true = 안잠김)
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
+    //비밀번호 만료 여부
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
+    // 계정 활성화 여부
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 
+    //계정 권한
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        Collection<GrantedAuthority> collectors = new ArrayList<>();
+
+
+        collectors.add(()-> "ROLE_"+user.getRole());
+        return collectors;
     }
 }
