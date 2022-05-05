74fe1fe406d390f2a32191da153d0166fa793a5c
Mint-Candy95 <32154068@dankook.ac.kr>
2022-05-05 16:09:46 +0900

    글쓰기추가

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,7 +1,5 @@
 package com.cos.blog.controller;
 
-import com.cos.blog.auth.PrincipalDetail;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,10 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BoardController {
 
     @GetMapping({"","/"})
-    public String index(@AuthenticationPrincipal PrincipalDetail principal){ //컨트롤러에서 세션을 어떻게 찾는지 ?
+    public String index(){ //컨트롤러에서 세션을 어떻게 찾는지 ?
         // /WEB-INF/veiws/index.jsp
-        System.out.printf("로그인 사용자 아이디 :"+principal.getUsername());
         return "index";
     }
+
+    //USER 권한이 필요
+    @GetMapping("/board/saveForm")
+    public String saveForm() {
+        return "board/saveForm";
+    }
+
 
 }
